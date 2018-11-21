1b71b91dc835e19b0b43b46be135dd463a3ab2aa
sxnan <suxuannan95@gmail.com>
2018-11-20 22:54:06 -0500

    Seperate main and astart

--- newastarheap/AStarAlgorithm.cpp
+++ newastarheap/AStarAlgorithm.cpp
@@ -11,47 +11,61 @@ constexpr int SIZE = 512;
 
 #define abs(x) ((x)>=0?(x):-(x))
 
+int astar(Map &map, int start_col, int start_row, int dest_col, int dest_row);
+
 int main(int argc, char *argv[])
 {
 	Map map("maze512-1-0.map");
-	//Map map("testmap.map");
-	//int rows = map.GetRowNum();
-	int cols = map.GetColNum();
-	int* indexptr = map.GetIndexAddr();
-	int* connectptr = map.GetConnectAddr();
-	//priority_queue<struct Grid *, vector<struct Grid *>, cmp> openlist;
-	Heap openlist;
-	bool closed[SIZE * SIZE] = {0};
-	//int start_row = 213, start_col = 67, dest_row = 231, dest_col = 87;    //114
-	//int start_row = 0, start_col = 3, dest_row = 1, dest_col = 0;    //35
-	//int start_row = 131, start_col = 5, dest_row = 213, dest_col = 154;    //3531
 	int start_col = atoi(argv[1]);
 	int start_row = atoi(argv[2]);
 	int dest_col = atoi(argv[3]);
 	int dest_row = atoi(argv[4]);
+
+    int shortestlength = astar(map, start_col, start_row, dest_col, dest_row);
+
+    if (shortestlength != -1) {
+        cout << "Shortest Pathlength : " << shortestlength << endl;
+    } else {
+        cout << "unreachable\n";
+    }
+
+	return 0;
+}
+
+/**
+ * Use astar to find the length of the shortest path from start to dest,
+ * return -1 if the dest is not reachable from
+ */
+int astar(Map &map, int start_col, int start_row, int dest_col, int dest_row) {
+
+	int cols = map.GetColNum();
 	int start_id = start_row * SIZE + start_col;
 	int dest_id = dest_row * SIZE + dest_col;
-	struct Grid * start_ptr = (struct Grid *) malloc(sizeof(struct Grid));
+	int* indexptr = map.GetIndexAddr();
+	int* connectptr = map.GetConnectAddr();
+	bool closed[SIZE * SIZE] = {0};
+	int shortestlength = 0;
+
+	Heap openlist;
+
+	struct Grid *start_ptr = (struct Grid *) malloc(sizeof(struct Grid));
 	start_ptr->id = start_id;
 	start_ptr->cost = 0;
 	start_ptr->prev_length = 0;
-	int shortestlength = 0;
-	openlist.push(start_ptr);
-	//vector<int> connections;
 
-	//AStar
+	openlist.push(start_ptr);
+
 	while (true)
 	{
-		//connections.clear();
 		if (openlist.size() == 0)
 		{
-			cout << "Empty openlist! " << endl;
-			break;
+			//cout << "Empty openlist! " << endl;
+            return -1;
 		}
 		int current_id = openlist.top()->id;
 		if (current_id == dest_id)
 		{
-			cout << "Shortest Pathlength : " << openlist.top()->prev_length << endl;
+			//cout << "Shortest Pathlength : " << openlist.top()->prev_length << endl;
 			shortestlength = openlist.top()->prev_length;
 			while (openlist.size() != 0)
 			{
@@ -66,47 +80,20 @@ int main(int argc, char *argv[])
 		free(openlist.top());
 		openlist.pop();
 		closed[current_id] = true;
-		// map.GetConnect(current_id, connections); 
-		// for (auto iter = connections.begin(); iter != connections.end(); ++iter)
-		// {
-		// 	if (closed[*iter] != 1)
-		// 	{
-		// 		struct Grid * grid_ptr = (struct Grid *) malloc(sizeof(struct Grid));
-		// 		int manh_dis = abs(*iter / cols - dest_row) + abs(*iter % cols - dest_col);
-		// 		grid_ptr->id = *iter;
-		// 		grid_ptr->prev_length = current_length + 1;
-		// 		grid_ptr->cost = current_length + 1 + manh_dis;
-		// 		openlist.push(grid_ptr);
-		// 	}
-		// }
-
-		//cout << indexptr[current_id] << " * " << indexptr[current_id + 1] << endl;
-		//cout << connectptr[0] << " ! " << connectptr[1] << endl;
 		for (int iter = indexptr[current_id]; iter < indexptr[current_id + 1]; ++iter) //TODO: SIMD
 		{
 			if (closed[connectptr[iter]] != 1)
 			{
-				//cout << connectptr[iter] << ' ';
 				struct Grid *grid_ptr = (struct Grid *)malloc(sizeof(struct Grid));
 				int manh_dis = abs(connectptr[iter] / cols - dest_row) + abs(connectptr[iter] % cols - dest_col);
 				grid_ptr->id = connectptr[iter];
 				grid_ptr->prev_length = current_length + 1;
 				grid_ptr->cost = current_length + 1 + manh_dis;
-				//grid_ptr->prev_id = current_id;
 				map.RecordPath(current_id, grid_ptr->id);
 				openlist.push(grid_ptr);
 			}
 		}
-		//cout << endl;
-	}
-	if(shortestlength != 0)
-	{
-		map.PrintPath(start_id, dest_id);
-		map.PrintMap();
 	}
 
-	//cout << map.GetRowNum() << endl;
-	//cout << map.GetColNum() << endl;
-
-	return 0;
+    return shortestlength;
 }
