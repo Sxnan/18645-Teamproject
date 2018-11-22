d133716cae6556ba0731f63097d59219c117c0d2
Qingsheng Ren <qren@andrew.cmu.edu>
2018-11-21 22:52:33 -0800

    Change the binary heap to 4-ary heap

--- newastarheap/heapbinary.cpp
+++ newastarheap/heapbinary.cpp
@@ -1,5 +1,4 @@
 #include "heap.h"
-#include <immintrin.h>
 
 Heap::Heap(void)
 {
@@ -23,15 +22,15 @@ void Heap::push(struct Grid *newgridptr)
 void Heap::heapify_up() {
     int now = heapsize;
     //while(data[now] <= data[now/2] && now != 1)
-    while(ptr[now]->cost <= ptr[(now + 2) / 4]->cost && now != 1)
+    while(ptr[now]->cost <= ptr[now/2]->cost && now != 1)
     {
         // int t = data[now];
         // data[now] = data[now/2];
         // data[now/2] = t;
         auto t = ptr[now];
-        ptr[now] = ptr[(now + 2) / 4];
-        ptr[(now + 2) / 4] = t;
-        now = (now + 2) / 4;
+        ptr[now] = ptr[now/2];
+        ptr[now / 2] = t;
+        now = now / 2;
     }
     return;
 }
@@ -49,19 +48,19 @@ void Heap::pop()
 
 void Heap::heapify_down() {
     int father = 1, child;
-    while(father * 4 - 2 <= heapsize)
+    while(father * 2 <= heapsize)
     {
-        child = father * 4 - 2;
-        int min_child = 100000, min_child_id = -1;
-        for (int i = 0; i < 4; i++)
-        {
-            if(child + i <= heapsize && ptr[child + i]->cost < min_child)
-            {
-                min_child = ptr[child + i]->cost;
-                min_child_id = i;
-            }
-        }
-        child += min_child_id;
+        child = 2 * father;
+        //if(child + 1 <= heapsize && data[child+1] < data[child]) child++;
+        if(child + 1 <= heapsize && ptr[child+1]->cost < ptr[child]->cost) child++;
+        // if(data[father] > data[child])
+        // {
+        //     int t = data[father];
+        //     data[father] = data[child];
+        //     data[child] = t;
+        //     father = child;
+        //     child = father * 2;
+        // }
         if(ptr[father]->cost > ptr[child]->cost)
         {
             auto t = ptr[father];
---
+++ newastarheap/heapbinary.h
@@ -0,0 +1,22 @@
+#include "grid.h"
+constexpr int MAXHEAPSIZE = 512 * 512;
+
+class Heap
+{
+private:
+    //int data[MAXHEAPSIZE];
+    struct Grid *ptr[MAXHEAPSIZE];
+    int heapsize;
+    void heapify_up();
+    void heapify_down();
+
+public:
+    Heap(void);
+    int size();
+    //void push(int newdata);
+    void push(struct Grid *newgridptr);
+    void pop();
+    struct Grid* top(void);
+    //int GetData(int index);
+    //void Output();
+};
