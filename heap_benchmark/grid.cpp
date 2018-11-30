cc52f46bed74e9eb0dd8fe2e296ceb56dfd8f4ce
sxnan <suxuannan95@gmail.com>
2018-11-30 13:28:29 -0500

    Heap Benchmarking

---
+++ heap_benchmark/grid.cpp
@@ -0,0 +1,37 @@
+#include "grid.h"
+//constexpr auto MAX_COST = 100000;
+
+/*
+Grid::Grid(void)
+{
+	closed = false;
+	cost = MAX_COST;
+	prev_cost = MAX_COST;
+	estimated_cost = MAX_COST;
+}
+
+Grid::Grid(int x, int y)
+{
+	cost = MAX_COST;
+	prev_cost = MAX_COST;
+	estimated_cost = MAX_COST;
+}
+
+bool Grid::operator<(const Grid & a) const
+{
+	// Smaller element has higher priority
+	return cost > a.cost;
+}
+
+
+void Grid::SetPriority(int p)
+{
+	cost = p;
+	return;
+}
+
+int Grid::GetCost(void) const
+{
+	return cost;
+}
+*/
